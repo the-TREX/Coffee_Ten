9facf4bfef085b50ea9fab81e53de28415c314c1
iliya <trex051.yonik@gmail.com>
2025-06-11 13:31:30 +0330

    1.0.2

--- Products/urls.py
+++ Products/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-    path('about/', views.about_us, name='about'),
-    path('shop/', views.all_products, name='shop'),
-    path('category/<slug:slug>/', views.category_products, name='category_products'),
-    path('<slug:slug>/', views.detail_products, name='detail_products'),
+    path('about/', views.About_us.as_view(), name='about'),
+    path('shop/', views.All_Products.as_view(), name='shop'),
+    path('category/<slug:slug>/', views.CategoryProductsView.as_view(), name='category_products'),
+    path('<slug:slug>/', views.Detail_Product.as_view(), name='detail_products'),
 
 ]
