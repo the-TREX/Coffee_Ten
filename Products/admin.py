1c564568f4ccc4f0826c0c265d2bba416377159a
iliya <trex051.yonik@gmail.com>
2025-06-22 19:51:36 +0330

    main

--- Products/admin.py
+++ Products/admin.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from .models import Categories, Products, Comment
 
 
-# @admin.register(Products)
-# @admin.register(Comment)
-# @admin.register(Categories)
-# class CategoryAdmin(admin.ModelAdmin):
-#     list_display = ['name', 'slug']
-#     prepopulated_fields = {'slug': ('name',)}
 
-admin.site.register(Products)
-admin.site.register(Categories)
+@admin.register(Categories)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug' , 'created_at' , 'breed']
+# admin.site.register(Products)
+# admin.site.register(Categories)
 admin.site.register(Comment)
