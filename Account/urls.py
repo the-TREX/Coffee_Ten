20056d5a8c63bb0114e631104c3cf2a99970d1aa
iliya <trex051.yonik@gmail.com>
2025-07-09 14:40:10 +0330

    Updateusermodel_2

--- Account/urls.py
+++ Account/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from . import views
+from .views import OTPLoginView, OTPVerifyView
 
 app_name = 'account'
-urlpatterns = [
 
-    path('register/', views.UserRegisterView.as_view(), name='register'),
-    path('login/', views.UserLoginView.as_view(), name='login'),
-    path('logout/', views.user_logout, name='logout'),
-    path('edit_profile/<int:pk>', views.UserEditeView.as_view(), name='edit_profile'),
-    path('contact_us/', views.ContactView.as_view(), name='contact_us'),
+urlpatterns = [
+    path('login/', OTPLoginView.as_view(), name='login'),
+    path('verify/', OTPVerifyView.as_view(), name='verify_otp'),
 ]
